255970ad5c6b915450a008d4c68532046d89bb45
Denis Krinitsyn <skylinekrin@gmail.com>
2022-12-10 12:38:14 +0500

    Task2 done

---
+++ Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+//Вызываем метод расчета сыммы чисел, в аргумент которого передаем число и метод расчета количество цифр в числе
+SumOfDigits(num, GetCalcSize(num));
+
+//Метод расчета длины числа
+int GetCalcSize(int num)
+{
+   int count = 0;
+    while(num > 0)
+    {
+        num /= 10;
+        count++;
+    } 
+    return count;
+}
+
+//Метод расчета суммы чисел
+void SumOfDigits(int num, int count)
+{    
+    int sum = 0;
+    for (int i = 0; i < count; i++)
+    {
+        sum += num%10;        
+        num /= 10;
+    }
+    Console.WriteLine(sum);
+}
