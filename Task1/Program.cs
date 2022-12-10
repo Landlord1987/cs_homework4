75ad7971e456aa80b37ae1952bdf3661d703d3cf
Denis Krinitsyn <skylinekrin@gmail.com>
2022-12-10 12:18:20 +0500

    Task1 done

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень возведения числа: ");
+int degree = Convert.ToInt32(Console.ReadLine());
+
+Exponentiation(num, degree);
+
+void Exponentiation(int num, int degree)
+{    
+    int result = 1;
+    for (int i = 0; i < degree; i++)
+    {
+        result *= num;
+    }
+    Console.WriteLine(result);
+}
